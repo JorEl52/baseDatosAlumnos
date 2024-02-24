3686ebf43fcfc74f24b4b9d4d132b768117cb6ed
Jair Hernandez <jahef5181@gmail.com>
2024-02-24 07:35:31 -0600

    Solo agregue el script del main.js y el listado de alumnos generando una tabla

---
+++ mainGrupos.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+//===============================Pestaña Alumnos======================///
+const Alumnos = document.getElementById('pestanaAlumnos')
+
+Alumnos?.addEventListener('click', function(event){
+    event.preventDefault()
+
+    //Al dar click en la pestaña Alumnos va a dirigir a a otra pagina
+    window.location.href="alumnos.html"
+})
+
+//==========================Volver a Inicio==============================///
+const inicio = document.getElementById('pestanaInicio')
+
+inicio?.addEventListener('click',function(event){
+    event.preventDefault()
+
+    //Al dar click en la pestaña inicio se recarga la pagina si esta en la pestaña principal
+    //si no lo está, simplemente se hace una redirección a la misma página
+    window.location.href = "index.html";
+})
