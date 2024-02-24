09f9e9adf56534d4ecd39f0e37b4c5063287c630
Jair Hernandez <jahef5181@gmail.com>
2024-02-23 19:15:22 -0600

    Agrego funciones cuando se da click en las opciones del navbar

--- main.js
+++ main.js
@@ -68,3 +68,33 @@ document.addEventListener( 'DOMContentLoaded', function () {
     });
     
 })
+
+//===============================Pestaña Alumnos======================///
+const Alumnos = document.getElementById('pestanaAlumnos')
+
+Alumnos?.addEventListener('click', function(event){
+    event.preventDefault()
+
+    //Al dar click en la pestaña Alumnos va a dirigir a a otra pagina
+    window.location.href="alumnos.html"
+})
+
+//=============================Pestaña Grupos=======================/////
+const Grupos = document.getElementById('pestanaGrupos')
+
+Grupos?.addEventListener('click',function(event){
+    event.preventDefault()
+    //Al dar click en la pestaña grupos te dirige a otra página
+    window.location.href = "grupos.html"
+})
+
+//==========================Volver a Inicio==============================///
+const inicio = document.getElementById('pestanaInicio')
+
+inicio?.addEventListener('click',function(event){
+    event.preventDefault()
+
+    //Al dar click en la pestaña inicio se recarga la pagina si esta en la pestaña principal
+    //si no lo está, simplemente se hace una redirección a la misma página
+    window.location.href = "index.html";
+})
