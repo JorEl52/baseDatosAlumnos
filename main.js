07da025cf35e0b636d0b75fa29fa563df90e1c53
Jair Hernandez <jahef5181@gmail.com>
2024-02-25 18:07:42 -0600

    Obtención del formulario y guardado de los datos en la memoria

--- main.js
+++ main.js
@@ -7,55 +7,29 @@ class Alumno {
         this.edad = edad;
         this.id = Alumno.contador++;
     }
-
-    ////------Métodos---------------///
-    /*//Funcion para guardar alumno
-    static guardarAlumno(alumno) {
-        localStorage.setItem("alumno", JSON.stringify(alumno));
-    }
-
-    //Función para cargar el alumnos del localStorage
-    static  cargarAlumno() {
-        var alumno = localStorage.getItem("alumno");
-        if(alumno) {
-            return JSON.parse(alumno);
-        }else{
-            return null;
-        }
-    } */
  
-    ///metodo test-borrar si es necesario
-    //Funcion para guardar alumno
+    //Funcion para guardar alumno en el local storage
     static guardarAlumno(alumno,index) {
+        for(index=1;index<=localStorage.length;index++){
+            console.log(alumno);
+            if(!localStorage.getItem("alumno" + index)){
+                break;
+            }
+        }
         localStorage.setItem(`alumno${index}`, JSON.stringify(alumno));
     }
 
-    //Función para cargar el alumnos del localStorage
-    static  cargarAlumnos() {
-        let alumnos = [];
-        for(var i=1;i<=localStorage.length;i++){
-            var alumnoData = localStorage.getItem("alumno" + i)
-            if(alumnoData){
-                var alumnoObject = JSON.parse(alumnoData)
-                var alumno = new Alumno(alumnoObject.nombre,alumnoObject.primerApellido,alumnoObject.segundoApellido,alumnoObject.edad)
-                alumnos.push(alumno)
-            }
-        }
-        return alumnos
-    }//hasta aqui llega el test
 
     //Mostrar el nombre
     mostrarNombre(){
-        return  this.id + '.- ' + this.primerApellido + " " + this.segundoApellido + " " + this.nombre;
+        return  this.id + '.- ' + this.primerApellido + " " + this.segundoApellido + " " + this.nombre + "----------" + this.edad;
     }
 
     static contador = 1;
 
 }
 
-
-
-
+//Funciones del formulario de inscripción
 document.addEventListener( 'DOMContentLoaded', function () {
     const registro =  document.getElementById("formularioRegistro")
     // Agregar un event listener al formulario de registro
@@ -71,18 +45,11 @@ document.addEventListener( 'DOMContentLoaded', function () {
         // Crear un nuevo objeto Alumno
         const alumno = new Alumno(nombre, primerApellido, segundoApellido, edad);
 
-        //test-guardar alumno en el localStorage, borrar si es necesario
-        for(index=1;index<=localStorage.length;index++){
-            if(!localStorage.getItem("alumno" + index)){
-                break;
-            }
-        }//hasta aqui llega el test
-
         // Guardar el alumno en localStorage
         Alumno.guardarAlumno(alumno);
+         
 
-
-        // Agregar el nombre del alumno a la lista y mostrar
+        // Agregar el nombre del alumno a la lista debajo del formulario de inscripión y mostrar
         var lista = document.getElementById("listaAlumnos");
         var nuevoAlumno = document.createElement("li");
         nuevoAlumno.textContent = alumno.mostrarNombre();
@@ -94,45 +61,19 @@ document.addEventListener( 'DOMContentLoaded', function () {
         //Limpiar los campos del formulario
         document.getElementById("formularioRegistro").reset();
     });
-    
-})
 
-//cargar alumnos test-borrar si es necesario
-// Cargar los alumnos almacenados en el local storage al cargar la página
-window.addEventListener("load", function() {
-    const alumnos = Alumno.cargarAlumnos();
-    for(let i=0; i<alumnos.length; i++) {
-        const alumno = alumnos[i];
-        const li = document.createElement("li");
-        li.textContent = alumno.mostrarNombre();
-        listaAlumnos.appendChild(li);
-    }
+    
 });
 
-//funcion de prueba-borrar si es necesario
-//funcion que cargara los alumnos del local storage y los mostrara en la tabla de la pestaña alumnos
-function cargarAlumnosDesdeLocalStorage() {
-    const alumnos = JSON.parse(localStorage.getItem('alumnos'));
-    if(alumnos) {
-        mostrarAlumnos(alumnos);
-    }
-}
-
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////Eventos del navbar, para rediccionar a otras paginas/////////////////////////////////////////////////////////////////////////
+///////////////////////////////////Eventos del navbar, para redireccionar a otras paginas/////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //===============================Pestaña Alumnos======================///
 const Alumnos = document.getElementById('pestanaAlumnos')
 
 Alumnos?.addEventListener('click', function(event){
-    event.preventDefault();
-    
-    //enviar datos test-borrar si es necesario
-    // Al dar click en la pestaña Alumnos, obtener los alumnos almacenados en el local storage y enviarlos a la página de alumnos
-    //const alumnos = Alumno.cargarAlumnos();
-    //localStorage.setItem("alumnos", JSON.stringify(alumnos));
-    cargarAlumnosDesdeLocalStorage();
+    event.preventDefault()
+
     //Al dar click en la pestaña Alumnos va a dirigir a a otra pagina
     window.location.href="alumnos.html";
 })
